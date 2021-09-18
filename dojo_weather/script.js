7bf273ae32b78b99cda9b237a009bd560feb6405
trietthai <90355157+trietthai@users.noreply.github.com>
2021-09-18 16:49:54 -0700

    Add files via upload
    initial commit

---
+++ dojo_weather/script.js
@@ -0,0 +1,17 @@
+function showMessage(){
+    alert("Loading weather report...");
+}
+function convert(){
+    var x = document.getElementById("tempType").text;
+
+}
+function hide(id) {
+    var element = document.querySelector(id);
+    element.remove();
+}
+function hide() {
+    var element = document.querySelector(cookies);
+    element.remove();
+    req.innerText--;
+    connection.innerText++;
+}   
