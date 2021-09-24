1e044b5a831328468415c63f3d60a9a856f926ee
trietthai <90355157+trietthai@users.noreply.github.com>
2021-09-23 20:30:44 -0700

    Add files via upload
    initial commit

---
+++ codejo_assigment/array.js
@@ -0,0 +1,51 @@
+// 1. Always Hungry
+function alwaysHungry(arr) {
+    // your code here 
+}
+   
+alwaysHungry([3.14, "food", "pie", true, "food"]);
+// this should console log "yummy", "yummy"
+alwaysHungry([4, 1, 5, 7, 2]);
+// this should console log "I'm hungry"
+
+// 2. High Pass Filter
+function highPass(arr, cutoff) {
+    var filteredArr = [];
+    // your code here
+    return filteredArr;
+}
+var result = highPass([6, 8, 3, 10, -2, 5, 9], 5);
+console.log(result); // we expect back [6, 8, 10, 9]
+
+
+// 3. Better than average
+function betterThanAverage(arr) {
+    var sum = 0;
+    // calculate the average
+    var count = 0
+    // count how many values are greated than the average
+    return count;
+}
+var result = betterThanAverage([6, 8, 3, 10, -2, 5, 9]);
+console.log(result); // we expect back 4
+
+// 4. Array Reverse
+function reverse(arr) {
+    // your code here
+    return arr;
+}
+var result = reverse(["a", "b", "c", "d", "e"]);
+console.log(result); // we expect back ["e", "d", "c", "b", "a"]
+
+
+// 5. Fibonacci Array
+function fibonacciArray(n) {
+    // the [0, 1] are the starting values of the array to calculate the rest from
+    var fibArr = [0, 1];
+    // your code here
+    return fibArr;
+}
+   
+var result = fibonacciArray(10);
+console.log(result); // we expect back [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+
