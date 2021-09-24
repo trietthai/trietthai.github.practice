ea22b20dc46631f73d6c05707811c9e307435f69
trietthai <90355157+trietthai@users.noreply.github.com>
2021-09-23 21:15:28 -0700

    Update array.js

--- codejo_assigment/array.js
+++ codejo_assigment/array.js
@@ -1,19 +1,37 @@
 // 1. Always Hungry
 function alwaysHungry(arr) {
-    // your code here 
+    // your code here
+    var count =0;
+    for(var i=0; i<arr.length; i++){
+        if(arr[i] == "food")
+        {
+            console.log("yummy");
+            count ++;
+
+        }
+    } 
+    if(count ==0){
+        console.log("Im hurry!")
+    }
 }
-   
 alwaysHungry([3.14, "food", "pie", true, "food"]);
 // this should console log "yummy", "yummy"
 alwaysHungry([4, 1, 5, 7, 2]);
 // this should console log "I'm hungry"
 
+
 // 2. High Pass Filter
 function highPass(arr, cutoff) {
     var filteredArr = [];
     // your code here
-    return filteredArr;
-}
+   for (var i=0; i<arr.length; i++) {
+       if(arr[i] > cutoff){
+           filteredArr.push(arr[i]);
+       }
+   }
+   return filteredArr;    
+ }
+
 var result = highPass([6, 8, 3, 10, -2, 5, 9], 5);
 console.log(result); // we expect back [6, 8, 10, 9]
 
@@ -21,9 +39,18 @@ console.log(result); // we expect back [6, 8, 10, 9]
 // 3. Better than average
 function betterThanAverage(arr) {
     var sum = 0;
+    for (var i=0; i<arr.length; i++){
+        sum += arr[i];
+    }
     // calculate the average
+    var avg = sum / arr.length;
     var count = 0
     // count how many values are greated than the average
+    for(var i=0; i<arr.length; i++){
+        if(arr[i]> avg){
+            count ++;
+        }
+    }
     return count;
 }
 var result = betterThanAverage([6, 8, 3, 10, -2, 5, 9]);
